0537593012f3dedf24386be192f6a8df994c040a
Charlie Kim <charlie2@ualberta.ca>
2024-05-16 00:14:30 -0600

    demo: login

--- main.go
+++ main.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/a-h/templ"
 	"github.com/charliekim2/chatapp/templates"
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
@@ -32,14 +30,17 @@ func helloHandler(c echo.Context) error {
     return Render(c, 200, templates.Hello(name))
 }
 
+func loginHandler(c echo.Context) error {
+    return Render(c, 200, templates.Login())
+}
+
 
 func main() {
     app := pocketbase.New()
 
-    // serves static files from the provided public dir (if exists)
     app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-        e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
         e.Router.GET("/hello/:name", helloHandler)
+        e.Router.GET("/login", loginHandler)
         return nil
     })
 

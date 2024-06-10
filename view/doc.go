e75a5646078d51bf22e1f9c0c0fccbc91162bdbd
ckim <112927312+charliekim2@users.noreply.github.com>
2024-06-10 15:43:17 -0600

    fix: typo

--- view/doc.go
+++ view/doc.go
@@ -1,3 +1,3 @@
-// Supressing package complaints when running `go mod tidy`, etc
+// Suppressing package complaints when running `go mod tidy`, etc
 // Make sure to run `templ generate` before running the app
 package view
